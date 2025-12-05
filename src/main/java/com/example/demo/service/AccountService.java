ec6861f35078b9894dd3ad53a4e704c4c917ae73
malamd <dlalsdud0104@gmail.com>
2025-12-05 13:36:36 +0900

    feat: wrote initial logic of creating an account and login features
    아직 로직에 필요한 메소드들이 완전히 준비되지 않아 테스트 불가능

---
+++ src/main/java/com/example/demo/service/AccountService.java
@@ -0,0 +1,46 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+/**
+ *  실제 로그인, 회원 가입 서비스
+ */
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+    RepositorySomething userRepository;
+
+    /**
+     * 화원 가입 서비스
+     * @param user 신규 회원 dto
+     * @return 새로 등록된 신규 회원 dto
+     */
+    public User createAccount(User user){
+        isDuplicatedMailAddress(user);
+        User saved =  userRepository.save();
+        if(saved == null) throw new NoSuchElementException(ERRORMESSAGE); // 만약 저장에 문제가 있다면 오류 메시지 출력
+        return saved;
+    }
+    private void isDuplicatedMailAddress(User user){
+        if(userRepository.findByEmail(user.getEmail).isPresent()){
+            throw new IllegalArgumentException(ERRORMESSAGE);
+        }
+    }
+
+    public boolean login(Request request){
+        String email = request. get email;
+        String password = request. get password;
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException(ERRORMESSAGE));
+
+        return user.getPassword(password);
+    }
+
+
+
+}
