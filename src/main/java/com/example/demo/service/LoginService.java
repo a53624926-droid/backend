0dce2faac846333e333123b5bee536e9eb27a4c9
malamd <dlalsdud0104@gmail.com>
2025-12-05 14:30:55 +0900

    refactor: refactored AccountService into three separate classes

---
+++ src/main/java/com/example/demo/service/LoginService.java
@@ -0,0 +1,31 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.User;
+import com.example.demo.dto.LoginRequestDTO;
+import com.example.demo.dto.UserDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+    RepositorySomething userRepository;
+
+    public boolean login(LoginRequestDTO request){
+        String email = request.getUser_email();
+        String password = request.getPassword();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->  new IllegalArgumentException(ACCOUNT_SERVICE_ERROR_MESSAGE // 주어진 이메일이 없는 경우
+                        .EMAIL_NOT_FOUND.content()));
+
+        if (!password.equals(user.getPassword()))
+            throw new IllegalArgumentException(ACCOUNT_SERVICE_ERROR_MESSAGE.
+                    WRONG_PASSWORD.content()); // 패스워드가 맞지 않는 경우
+
+        return true;
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/service/SignInService.java
@@ -0,0 +1,27 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.User;
+import com.example.demo.dto.UserDTO;
+
+import java.util.NoSuchElementException;
+
+public class SignInService {
+    /**
+     * 화원 가입 서비스
+     * @param user 신규 회원 dto
+     * @return 새로 등록된 신규 회원 dto
+     */
+    public User createAccount(UserDTO user){
+        isDuplicatedMailAddress(user);
+        User saved =  userRepository.save();
+        if(saved == null) throw new NoSuchElementException(ACCO); // 만약 저장에 문제가 있다면 오류 메시지 출력
+        return saved;
+    }
+    private void isDuplicatedMailAddress(UserDTO user){
+        String address = user.getUser_email();
+        userRepository.findByEmail(address).isPresent(
+                ()-> new IllegalArgumentException(ACCOUNT_SERVICE_ERROR_MESSAGE. // 중복된 이메일인 경우
+                        DUPLICATED_ADDRESS.content()));
+
+    }
+}
